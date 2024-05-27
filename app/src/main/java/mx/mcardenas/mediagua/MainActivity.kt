a71033142ffd45570ec0c5646a5ea3a822adb216
Madjesc <mauricio.291202@gmail.com>
2024-05-27 16:02:35 -0600

    Created welcome activity

--- app/src/main/java/mx/mcardenas/mediagua/MainActivity.kt
+++ app/src/main/java/mx/mcardenas/mediagua/MainActivity.kt
@@ -7,11 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import mx.mcardenas.mediagua.ui.theme.AppTheme
-import mx.mcardenas.mediagua.ui.theme.AppTypography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +21,9 @@ class MainActivity : ComponentActivity() {
             AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(modifier = Modifier.padding(innerPadding)) {
-                        Text(text = "Bienvenuto", style = AppTypography.displayMedium)
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Entrar")
+                        }
                     }
                 }
             }
